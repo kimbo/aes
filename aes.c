44fc5a4416de7ecfda36d212f963d52276649307
kimbo <kimballleavitt@gmail.com>
2020-09-03 17:05:06 -0600

    add subWord function, stub out some other stuff

--- aes.c
+++ aes.c
@@ -39,7 +39,16 @@ void keyExpansion(uint8_t *key, uint32_t *buf)
 
 uint32_t subWord(uint32_t word)
 {
-	return 1;
+	uint32_t result = 0;
+	uint8_t byteNo;
+	for (byteNo = 0; byteNo < 4; byteNo++) {
+		uint8_t byte = (word >> (8 * byteNo) & 0xff);
+		uint8_t row = byte >> 4;
+		uint8_t col = byte & 0x0F;
+		uint8_t newByte = sbox[row][col];
+		result |= (newByte << (8 * byteNo));
+	}
+	return result;
 }
 
 uint32_t rotWord(uint32_t word)
@@ -47,3 +56,24 @@ uint32_t rotWord(uint32_t word)
 	return 1;
 }
 
+void mixColumns(uint8_t state[4][4])
+{
+	
+}
+
+void subBytes(uint8_t state[4][4])
+{
+
+}
+void shiftRows(uint8_t state[4][4])
+{
+
+}
+void addRoundKey(uint8_t state[4][4], uint32_t *w, uint8_t something)
+{
+
+}
+void cipher(uint8_t *in, uint8_t *out, uint32_t *w)
+{
+
+}
